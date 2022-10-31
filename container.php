ee8f9228e0fffb007b01e9f435a545a0f8bcc754
Carlos Trejo <carlostrejo182@gmail.com>
2022-10-30 22:09:01 -0600

    add chat Interface

---
+++ container.php
@@ -0,0 +1,25 @@
+</head>
+<body class="">
+<div role="navigation" class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav" id="ulNav">
+            <li class="active" id="logo" ><a href="https://www.iztapalapa.tecnm.mx/" target="_blank"><img id="logoItiz" src="./img/logo.png" alt="vinculo a ITIZ" style="width:95% ;"></a></li>
+            <li class="active" id="logo" ><a href="https://itiztapalapa1.mindbox.app/login/alumno" target="_blank"><img id="logoItiz" src="./img/mindbox.jpg" alt="vinculo a ITIZ" style="width:95% ;"></a></li>           
+          </ul>
+         
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+	
+	<div class="container" style="min-height:500px;">
+	<div class=''>
+	</div>
---
+++ login.php
@@ -0,0 +1,61 @@
+<?php 
+session_start();
+include('header.php');
+$loginError = '';
+if (!empty($_POST['username']) && !empty($_POST['pwd'])) {
+	include ('Chat.php');
+	$chat = new Chat();
+	$user = $chat->loginUsers($_POST['username'], $_POST['pwd']);	
+	if(!empty($user)) {
+		$_SESSION['username'] = $user[0]['username'];
+		$_SESSION['userid'] = $user[0]['userid'];
+		$chat->updateUserOnline($user[0]['userid'], 1);
+		$lastInsertId = $chat->insertUserLoginDetails($user[0]['userid']);
+		$_SESSION['login_details_id'] = $lastInsertId;
+		header("Location:index.php");
+	} else {
+		$loginError = "Usuario y Contraseña invalida";
+	}
+}
+
+?>
+<title>Sistema Chat/login.hph</title>
+<?php include('container.php');?>
+<div class="containerLogin">		
+	<h2>Sistema de chat del plan de estudios de  la carrera de ISC</h1>		
+	<div class="row">
+		<div class="col-sm-4">
+			<label style="font-size: larger;">Chat Login:</label>
+			<br>		
+			<form method="post">
+				<div class="form-group">
+				<?php if ($loginError ) { ?>
+					<div class="alert alert-warning"><?php echo $loginError; ?></div>
+				<?php } ?>
+				</div>
+				<div class="form-group">
+					<label for="username">Usuario:</label>
+					<input type="username" class="form-control" name="username" required style="text-align: center;">
+				</div>
+				<div class="form-group">
+					<label for="pwd">Contraseña:</label>
+					<input type="password" class="form-control" name="pwd" required style="text-align: center;">
+				</div>  
+				<br>
+				<button  type="submit" name="login" class="btnLogin">Iniciar Sesion</button>
+			</form>
+			<br>
+			<br>
+			<p><b>Usuario</b> : Smith<br><b>Password</b> : 123</p>
+
+		</div>
+		
+	</div>
+</div>	
+<?php include('footer.php');?>
+
+
+
+
+
+
